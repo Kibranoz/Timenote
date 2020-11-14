b184f6045b86e97011b075b8ea2c6d384eefad32
Louis <louisrcouture@protonmail.com>
2020-11-13 21:12:51 -0500

    Initial Commit

---
+++ Timenote/TimenoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimenoteApp.swift
+//  Timenote
+//
+//  Created by Louis Couture on 2020-11-13.
+//
+
+import SwiftUI
+
+@main
+struct TimenoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
